9605d5205755d0d97e264f9e9971f98fc66a7465
hwyi21 <chozza2019@naver.com>
2022-09-11 17:37:54 +0900

    [test] String split 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,22 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("String split 테스트")
+    void split() {
+        String example = "1,2";
+        String example1 = "1";
+        String[] splitArray = example.split(",");
+        String[] splitArray1 = example1.split(",");
+
+        //중복여부, 순서에 관계 없이 값만 일치하면 테스트가 성공
+        assertThat(splitArray).contains("1", "2");
+        assertThat(splitArray1).contains("1");
+
+        //순서를 포함해서 정확히 일치
+        assertThat(splitArray).containsExactly("1", "2");
+        assertThat(splitArray1).containsExactly("1");
     }
 }
