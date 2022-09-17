54fd9a7682bc70318a718255ba6833e0936667f9
heeom <swimxdev@gmail.com>
2022-09-17 15:58:02 +0900

    feat: Ball 구현

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Ball {
+    private final int position;
+    private final int ballNo;
+
+    public Ball(int position, int ballNo) {
+        this.position = position;
+        this.ballNo = ballNo;
+    }
+
+    public baseball.BallStatus play(Ball ball) {
+        if (this.equals(ball)) {
+            return BallStatus.STRIKE;
+        }
+        if (ball.matchBallNo(ballNo)) { // 필드에 직접 접근 -> 객체에 메세지를 보내는 방식
+            return BallStatus.BALL;
+        }
+        return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNo(int ballNo) {
+        return this.ballNo == ballNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && ballNo == ball.ballNo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, ballNo);
+    }
+}
