b7eefb51a135195209121ac16c179f424dd7c93e
heeom <swimxdev@gmail.com>
2022-09-18 22:32:40 +0900

    test : NumberGeneratorTest

---
+++ src/test/java/baseball/domain/NumberGeneratorTest.java
@@ -0,0 +1,18 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberGeneratorTest {
+    @Test
+    void generate_none_duplicate_numbers() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> randomNumbers =  numberGenerator.createRandomNumbers();
+        List<Integer> distinctResult = randomNumbers.stream().distinct().collect(Collectors.toList());
+
+        Assertions.assertThat(distinctResult.size()).isEqualTo(randomNumbers.size());
+    }
+}
