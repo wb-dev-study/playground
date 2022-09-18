590b54d5d566e70a61315fa42d1953c111ec7616
heeom <swimxdev@gmail.com>
2022-09-18 23:23:11 +0900

    test : BallGameTest

--- src/test/java/baseball/domain/BallGameTest.java
+++ src/test/java/baseball/domain/BallGameTest.java
@@ -1,21 +1,42 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class BallGameTest {
 
-    @Test
-    void play() {
-        BallGame ballGame = new BallGame(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2));
-        BallGameResult ballGameResult = ballGame.play();
-        assertThat(ballGameResult.getBall()).isEqualTo(2);
-        assertThat(ballGameResult.getStrike()).isEqualTo(1);
+    private List<Integer> computer;
+    @BeforeEach
+    void setUp() {
+        computer = Arrays.asList(1, 2, 3);
+    }
 
+    @Test
+    void strike_1_ball_2() {
+        BallGame ballGame = new BallGame(computer, Arrays.asList(1, 3, 2));
+        BallGameResult ballGameResult = ballGame.play();
+        assertThat(ballGameResult.isThreeStrike()).isFalse();
+        assertThat(ballGameResult.getResult()).isEqualTo("2 볼 1 스트라이크");
+    }
+
+    @Test
+    void strike_3() {
+        BallGame ballGame = new BallGame(computer, Arrays.asList(1, 2, 3));
+        BallGameResult ballGameResult = ballGame.play();
+        assertThat(ballGameResult.isThreeStrike()).isTrue();
+    }
+
+    @Test
+    void nothing() {
+        BallGame ballGame = new BallGame(computer, Arrays.asList(4, 5, 6));
+        BallGameResult ballGameResult = ballGame.play();
+        assertThat(ballGameResult.getResult()).isEqualTo("nothing!!");
     }
 
     @DisplayName("중복된 숫자가 있는 경우 예외 발생")
