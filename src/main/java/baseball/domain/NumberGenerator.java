d5bc6348069e9a29b5396c1ab2cc798747ef65fc
heeom <swimxdev@gmail.com>
2022-09-18 18:13:21 +0900

    feat : createRandomNumbers 구현

--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,9 +1,27 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
+    public static int MAX_SIZE = 3;
+    List<Integer> numbers = new ArrayList<>();
     public List<Integer> createRandomNumbers() {
-        return null;
+        while (!isMaxSize()) {
+            int number = getRandomNumber();
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
+    }
+
+    private int getRandomNumber() {
+        return new Random().nextInt(9) + 1;
+    }
+
+    private boolean isMaxSize() {
+        return numbers.size() == MAX_SIZE;
     }
 }
