4c06cec4a02e850d9d07e7d398b8ead218a76e72
heeom <swimxdev@gmail.com>
2022-09-18 17:13:23 +0900

    feat : baseball skeleton code

---
+++ src/main/java/baseball/domain/Judgement.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgement {
+    public int correctCount(List<Integer> answers, List<Integer> players) {
+        return 0;
+    }
+
+    public boolean hasPlace(int placeNumber, int number) {
+        return false;
+    }
+}
---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+    public String compare(List<Integer> answer, List<Integer> player) {
+        return "";
+    }
+}
