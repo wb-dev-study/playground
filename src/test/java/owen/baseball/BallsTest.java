339f4d1c092d94d798715a3ac2b0f0d26c9a7dfb
엄태권 <akfls221@gmail.com>
2022-09-19 01:34:46 +0900

    fix : test code 수정

--- src/test/java/owen/baseball/BallsTest.java
+++ src/test/java/owen/baseball/BallsTest.java
@@ -3,12 +3,12 @@ package owen.baseball;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import owen.baseball.domain.BallStatus;
 import owen.baseball.domain.ball.Ball;
 import owen.baseball.domain.ball.Balls;
 import owen.baseball.util.BallFactory;
-import owen.baseball.util.Result;
 
 import java.util.Arrays;
 
@@ -22,75 +22,21 @@ public class BallsTest {
     }
 
     @Test
-    void outOfRange_error() {
-        Assertions.assertThatThrownBy(() -> BallFactory.makeBall("012")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageMatching("1 ~ 9 까지의 정수만 입력이 가능합니다.");
-    }
-
-    @Test
-    void inputOtherType_error() {
-        Assertions.assertThatThrownBy(() -> BallFactory.makeBall("s@1")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageMatching("정수만 입력 가능합니다.");
-    }
-
-    @Test
-    void inputLength_error() {
-        Assertions.assertThatThrownBy(() -> BallFactory.makeBall("1112")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageMatching("3자리의 숫자를 입력 바랍니다.");
-
-        Assertions.assertThatThrownBy(() -> BallFactory.makeBall("11")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageMatching("3자리의 숫자를 입력 바랍니다.");
-    }
-
-    @Test
-    void duplicate_error() {
-        Assertions.assertThatThrownBy(() -> BallFactory.makeBall("111")).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageMatching("중복된 숫자는 입력 불가능 합니다.");
-    }
-
-    @Test
-    void test_Nothing() {
-        Balls userBall = BallFactory.makeBall("456");
-        Result result = answerBalls.play(userBall);
-        Assertions.assertThat(result.getStrike()).isEqualTo(0);
-        Assertions.assertThat(result.getBall()).isEqualTo(0);
-    }
-
-    @Test
-    void test_2ball1Strike() {
-        Balls userBall = BallFactory.makeBall("132");
-        Result result = answerBalls.play(userBall);
-        Assertions.assertThat(result.getStrike()).isEqualTo(1);
-        Assertions.assertThat(result.getBall()).isEqualTo(2);
-    }
-
-    @Test
-    void test_3strike() {
-        Balls userBall = BallFactory.makeBall("123");
-        Result result = answerBalls.play(userBall);
-        Assertions.assertThat(result.getStrike()).isEqualTo(3);
-        Assertions.assertThat(result.is3Strike()).isEqualTo(true);
-
-    }
-
-    @Test
+    @DisplayName("낫싱 테스트")
     void nothingTest() {
         Assertions.assertThat(answerBalls.playBall(new Ball(9, 1))).isEqualTo(BallStatus.NOTHING);
     }
 
     @Test
+    @DisplayName("스트라이크 테스트")
     void strikeTest() {
         Assertions.assertThat(answerBalls.playBall(new Ball(1, 1))).isEqualTo(BallStatus.STRIKE);
     }
 
     @Test
+    @DisplayName("볼 테스트")
     void ballTest() {
         Assertions.assertThat(answerBalls.playBall(new Ball(1, 3))).isEqualTo(BallStatus.BALL);
     }
 
-    @Test
-    void totalTest() {
-        Balls userBalls = new Balls(Arrays.asList(2, 1, 3));
-        Assertions.assertThat(answerBalls.playBall(new Ball(1, 3))).isEqualTo(BallStatus.BALL);
-    }
 }
