7d27c13420cbc523b841585b6384d112f443cfcd
jaejun1130 <wjswowns1234@gmail.com>
2022-09-20 21:45:30 +0900

    refactor: 스트라이크, 볼, 낫싱 클래스 분리

--- src/main/java/jay/baseball/BaseballGame.java
+++ src/main/java/jay/baseball/BaseballGame.java
@@ -5,27 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BaseballGame {
-    public int total(List<Integer> computer, List<Integer> player) {
-        int total = 0;
-        for (int i = 0; i < player.size(); i++) {
-            if (computer.contains(player.get(i))) {
-                total += 1;
-            }
-        }
-        return total;
-    }
-
-
-    public int strike(List<Integer> computer, List<Integer> player) {
-        int strike = 0;
-        for (int i = 0; i < player.size(); i++) {
-            if (computer.get(i).equals(player.get(i))) {
-                strike += 1;
-            }
-        }
-        return strike;
-    }
-
     public boolean retry() {
         System.out.println("축하합니다! 경기를 다시 시작하겠습니까? 다시 시작 : 1, 종료 : 2");
         Scanner scanner = new Scanner(System.in);
--- src/main/java/jay/baseball/Main.java
+++ src/main/java/jay/baseball/Main.java
@@ -5,18 +5,20 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
+        ComputerRandomNumber computerRandomNumber = new ComputerRandomNumber();
+        BaseballBatsman baseballBatsman = new BaseballBatsman();
         boolean retry = true;
 
         while (retry) {
             String result = "";
-            List<Integer> computer = baseballGame.create();     //컴퓨터 난수 생성
+            List<Integer> computer = computerRandomNumber.create();     //컴퓨터 난수 생성
             while (!result.equals("3스트라이크")) {
                 System.out.println("숫자를 입력해주세요");
                 List<Integer> player = baseballGame.player();   //플레이서 입력값
 
-                int total = baseballGame.total(computer, player);   //자리 상관없이 맞춘 횟수
-                int strike = baseballGame.strike(computer, player); //자리 까지 맞춘 횟수
-                int ball = total - strike;                          //볼
+                int total = baseballBatsman.total(computer, player);   //자리 상관없이 맞춘 횟수
+                int strike = baseballBatsman.strike(computer, player); //자리 까지 맞춘 횟수
+                int ball = baseballBatsman.ball(total,strike);         //볼
 
                 if (total == 0) {
                     result = "낫싱";
