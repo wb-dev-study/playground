914c1fb6d75be5bfe08db9a9b3cb444685c365a2
peter <sungjae89@gmail.com>
2022-09-23 10:03:00 +0900

    refactor: Ball 생성 시 validate 별도 처리

--- src/main/java/peter/baseball/domain/BallFactory.java
+++ src/main/java/peter/baseball/domain/BallFactory.java
@@ -2,8 +2,11 @@ package peter.baseball.domain;
 
 import peter.baseball.exception.InvalidNumberException;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 /**
@@ -13,13 +16,19 @@ public class BallFactory {
     public static Set<Ball> makeBalls(String input) {
 
         Set<Ball> balls = new LinkedHashSet<>();
+        List<Integer> split = convertIntArray(input);
+        for (Integer in : split) {
+            balls.add(new Ball(in));
+        }
+
+        return balls;
+    }
+
+    public static List<Integer> convertIntArray(String input) {
         try {
-            for (String in : input.split("")) {
-                balls.add(new Ball(Integer.parseInt(in)));
-            }
+            return Arrays.asList(input.split("")).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
         } catch (NumberFormatException e) {
             throw new InvalidNumberException();
         }
-        return balls;
     }
 }
