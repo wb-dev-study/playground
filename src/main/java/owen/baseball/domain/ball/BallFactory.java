48fe9e0fe3830aaf552319b0ef9ab33b5efaaa30
엄태권 <akfls221@gmail.com>
2022-09-24 05:06:23 +0900

    fix : for문 - > stream으로 변환

--- src/main/java/owen/baseball/domain/ball/BallFactory.java
+++ src/main/java/owen/baseball/domain/ball/BallFactory.java
@@ -1,20 +1,26 @@
-package owen.baseball.util;
+package owen.baseball.domain.ball;
 
 
 import owen.baseball.domain.ball.Balls;
+import owen.baseball.util.Validate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class BallFactory {
 
+    public static final Validate validate = new Validate();
+
+    public static final Random random = new Random();
+
     public static Balls computerBall() {
         Set<Integer> randomSet = new LinkedHashSet<>();
-        Random random = new Random();
         while (randomSet.size() < 3) {
             randomSet.add(random.nextInt(9) + 1);
         }
@@ -23,16 +29,9 @@ public class BallFactory {
     }
 
     public static Balls makeBall(String userInput) {
-        Validate validate = new Validate();
-
         String[] splitInput = userInput.split("");
         validate.validCheck(splitInput);
-        List<Integer> userBallList = new ArrayList<>();
-
-        for (String inputString : splitInput) {
-            userBallList.add(Integer.valueOf(inputString));
-        }
-
+        List<Integer> userBallList = Arrays.stream(splitInput).map(Integer::parseInt).collect(Collectors.toList());
         return new Balls(userBallList);
     }
 }
