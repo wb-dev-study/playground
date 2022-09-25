7bc613945eda02a3ee0ffb8ad3cf99716e24eabc
jaejun1130 <wjswowns1234@gmail.com>
2022-09-25 16:09:53 +0900

    test: car move winner 테스트 코드 작성

--- src/test/java/jay/racingcar/domain/CarTest.java
+++ src/test/java/jay/racingcar/domain/CarTest.java
@@ -1,6 +1,7 @@
 package jay.racingcar.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,5 +20,28 @@ class CarTest {
                 new Car(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5글자 이하로만 입력가능합니다.");
+    }
+
+    @Test
+    public void carMoveTest() {
+        Car car = new Car("a");
+        car.move(4);
+        car.move(3);
+
+        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void carWinnerTest() {
+        Car carA = new Car("a");
+        carA.move(4);
+
+        Car carB = new Car("b");
+        carB.move(3);
+
+        Car carC = new Car("c");
+        carC.move(2);
+
+        Assertions.assertThat(carA.isWinner(1)).isTrue();
     }
 }
