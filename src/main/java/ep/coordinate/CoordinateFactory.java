98d15e2a3702ba02d0b4e63ccd76c49f9ed508c3
EP <kjuioqq8@gmail.com>
2022-10-02 13:41:47 +0900

    refactor: 팩토리 객체 static으로 inmemory 에 저장

--- src/main/java/ep/coordinate/CoordinateFactory.java
+++ src/main/java/ep/coordinate/CoordinateFactory.java
@@ -7,31 +7,28 @@ import ep.coordinate.figure.factory.TriangleFactory;
 import ep.coordinate.figure.material.Position;
 import ep.coordinate.figure.material.Vertex;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CoordinateFactory {
 
     private final Vertex vertex;
-    private final FigureFactory figureFactory;
+    private static final Map<Integer, FigureFactory> figureFactoryMap = new HashMap<>();
+
+    static {
+        figureFactoryMap.put(3, new TriangleFactory());
+        figureFactoryMap.put(4, new RectangleFactory());
+    }
 
     public CoordinateFactory(List<Position> positions) {
         this.vertex = new Vertex(positions);
-        switch (this.vertex.getVertexSize()) {
-            case 3: {
-                this.figureFactory = new TriangleFactory();
-                break;
-            }
-            case 4: {
-                this.figureFactory = new RectangleFactory();
-                break;
-            }
-            default: {
-                throw new IllegalArgumentException("해당하는 꼭지점의 도형이 구현이되지 않았습니다.");
-            }
+        if (figureFactoryMap.get(this.vertex.getVertexSize()) == null) {
+            throw new IllegalArgumentException("해당하는 꼭지점 개수의 도형이 구현이되지 않았습니다.");
         }
     }
 
     public Figure figure() {
-        return this.figureFactory.vertexToFigure(this.vertex);
+        return figureFactoryMap.get(vertex.getVertexSize()).vertexToFigure(this.vertex);
     }
 }
